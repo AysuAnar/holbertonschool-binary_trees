2c62892e82f0d5485bfb2040672509caeaead0f4
AysuAnar <you@example.com>
2024-04-08 14:50:41 +0000

    task0:fixing

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new->n = value;
 	new->parent = parent;
-	new->parent = parent; 	
+	new->parent = parent;	
 	new->left = NULL;
 	new->right = NULL;
 
